d21dbe9dee529f97c2e95cd2f867254445c97f50
EdwardHuCS <edward.s.hu.cs@gmail.com>
2015-10-22 22:27:58 -0700

    AND HIS NAME IS JOHN CENA

---
+++ rah.php
@@ -0,0 +1,7 @@
+<?php
+$pref = $_GET['pref'];
+if($pref == 4)
+{
+	echo "<img src='http://c.jcket.com/stoneyroads/files/2015/09/600x/John-Cena.jpg'/> < br/> <img src = 'http://i.ytimg.com/vi/TUQ3nVoN-ko/maxresdefault.jpg /> < br /> <img src = 'http://www.quickmeme.com/img/26/260e24f735bd6ca4c5e3a1815b9986bf14fb14c25446db674a24b877a91c82cb.jpg' />";
+}
+?>
