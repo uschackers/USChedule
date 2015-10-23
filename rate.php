60fad8b6f76912f4b9cfa83aa81b9cf755361b61
EdwardHuCS <edward.s.hu.cs@gmail.com>
2015-10-23 15:11:30 -0700

    Updated rate my prof
    now sorts professors by rating

--- rate.php
+++ rate.php
@@ -1,20 +1,28 @@
+
 <?php
 
 $url = "http://www.ratemyprofessors.com/find/professor/?department=&institution=&page=1&query=*%3A*&queryoption=TEACHER&queryBy=schoolId&sid=1381&sortBy=";
 $json = file_get_contents($url);
 $data = json_decode($json,true);
-for($i = 0; $i < sizeof($data["professors"]); $i++)
-{
-	$professor = $data["professors"][$i];
-	//name
-	echo "Professor: ".$professor["tFname"]." ".$data["professors"][$i]["tLname"];
-	
-	//overall rating
-	echo " Rating: ".$professor["overall_rating"];
+$professor = $data["professors"];
 
-	//line break
-	echo "<br>";
-}
+// for($i = 0; $i < sizeof($data["professors"]); $i++)
+// {
+// 	$professor = $data["professors"][$i];
+	
+// 	//overall rating
+// 	echo " Rating: ".$professor["overall_rating"];
+
+// 	//line break
+// 	echo "<br>";
+// }
+//$array = json_decode($professor,true);
+usort($professor, function($a,$b){
+
+	return (int)$a["overall_rating"] > (int)$b["overall_rating"] ? -1 : 1;
+});
+
+echo json_encode($professor,JSON_PRETTY_PRINT);
 
 
 
